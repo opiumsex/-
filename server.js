41404b261ae2199b93dddb8530398e13139f11a7
opiumsex <d46962085@gmail.com>
2025-09-06 21:45:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,27 +5,55 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
 // Статическая директория для фронтенда
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Маршрут для главной страницы
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Хранение сообщений (в памяти)
+let messages = [];
+
 // Обработка подключений
 io.on('connection', (socket) => {
-    console.log('Новый пользователь подключен');
+    console.log('Новый пользователь подключен:', socket.id);
+
+    // Отправка истории сообщений новому пользователю
+    socket.emit('messageHistory', messages);
 
     // Получение сообщения от клиента
     socket.on('sendMessage', (data) => {
-        // Отправка сообщения всем подключенным клиентам
-        io.emit('newMessage', {
+        const messageData = {
+            id: Date.now(),
             username: data.username,
             message: data.message,
             timestamp: new Date().toLocaleTimeString()
-        });
+        };
+
+        // Сохраняем сообщение
+        messages.push(messageData);
+        
+        // Ограничиваем историю последними 100 сообщениями
+        if (messages.length > 100) {
+            messages = messages.slice(-100);
+        }
+
+        // Отправка сообщения всем подключенным клиентам
+        io.emit('newMessage', messageData);
+        console.log('Сообщение отправлено:', messageData);
     });
 
     socket.on('disconnect', () => {
-        console.log('Пользователь отключен');
+        console.log('Пользователь отключен:', socket.id);
     });
 });
 
